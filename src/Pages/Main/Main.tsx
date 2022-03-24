3bb871748283ededb00850977da5546fec80a785
huynguyen52 <huynguyen5212@gmail.com>
2022-03-24 09:25:09 +0700

    fix font variable

--- src/Pages/Main/Main.tsx
+++ src/Pages/Main/Main.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
+import Header from '../../Component/Header/Header';
 import TextUnderline from '../../Component/TextUnderline/TextUnderline';
 
 function Main(props: any) {
-  const data =
-    'Awwards Conference — Amsterdam. Awwards Conference — San Francisco. IngeniUX — Seattle. Digital Design Days 2019 — Milan. Framer Loupe 2018 — Amsterdam';
-
   return (
     <>
-      {data.split('.').map((item, index) => (
-        <div>
-          <TextUnderline bottom={0} key={index}>
-            {item}.
-          </TextUnderline>
-          <br />
-        </div>
-      ))}
-      {/* <TextUnderline /> */}
+      <Header />
     </>
   );
 }
