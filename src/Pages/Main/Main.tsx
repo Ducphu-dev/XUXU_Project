d2942641b79498483e3d769bf1b326a735e52daf
DucPhu <ducphu.170720@gmail.com>
2022-03-28 13:55:09 +0700

    fix bug

--- src/Pages/Main/Main.tsx
+++ src/Pages/Main/Main.tsx
@@ -6,19 +6,23 @@ import HomePageSec3 from '../../Component/HomePageSec/Sec3/HomePageSec3';
 import HomePageSec4 from '../../Component/HomePageSec/Sec4/HomePageSec4';
 import HomePageSec5 from '../../Component/HomePageSec/Sec5/HomePageSec5';
 import HomePageSec6 from '../../Component/HomePageSec/Sec6/HomePageSec6';
+import { BackTop } from 'antd';
+import iconBack from "../../Assets/images/HomePage/36-px-bk-scrup@2x.png"
 
 import Header from '../../Component/Header/Header';
 import TextUnderline from '../../Component/TextUnderline/TextUnderline';
 
 function Main(props: any) {
+ 
   return (
     <>
-      <HomePageSec1/>
-      <HomePageSec2/>
-      <HomePageSec3/>
-      <HomePageSec4/>
-      <HomePageSec5/>
-      <HomePageSec6/>
+        <HomePageSec1/>
+        <HomePageSec2/>
+        <HomePageSec3/>
+        <HomePageSec4/>
+        <HomePageSec5/>
+        <HomePageSec6/>
+        
     </>
   );
 }
